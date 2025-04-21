7e85745d8b3b4e576081073ff72741080f19874d
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-04-21 11:50:00 +0200

    reduce PR to minimum

--- R/stack.R
+++ R/stack.R
@@ -67,22 +67,6 @@ get_bootstrap_stack <- function(longdata, method, stack = Stack$new()) {
 #' @param longdata A [longDataConstructor()] object
 #' @param method A `method` object
 #' @param stack A [Stack()] object (this is only exposed for unit testing purposes)
-get_jackknife_stack1 <- function(longdata, method, stack = Stack$new()) {
-    ids <- longdata$ids
-    stack$add(
-        lapply(seq_along(ids), function(i) ids[-i])
-    )
-    return(stack)
-}
-
-#' Creates a stack object populated with jackknife samples
-#'
-#' Function creates a [Stack()] object and populated the stack with jackknife
-#' samples based upon
-#'
-#' @param longdata A [longDataConstructor()] object
-#' @param method A `method` object
-#' @param stack A [Stack()] object (this is only exposed for unit testing purposes)
 get_jackknife_stack <- function(longdata, method, stack = Stack$new()) {
     ids <- longdata$ids
     cluster <- longdata$cluster
